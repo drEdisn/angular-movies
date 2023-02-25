9a5379bc1997a6c7e58a308753130ce8467d2bab
drEdisn <denizs00@mail.ru>
2023-02-25 19:06:08 +0300

    changed tests

--- src/app/shared/services/api.service.spec.ts
+++ src/app/shared/services/api.service.spec.ts
@@ -1,4 +1,4 @@
-import { testMovies, testGenres } from '../../../assets/mock/testData.mock';
+import { testMoviesMock, testGenresMock } from '../../../assets/mock/testData.mock';
 import { HttpClientModule } from '@angular/common/http';
 import { ApiService } from './api.service';
 import { TestBed } from '@angular/core/testing';
@@ -20,22 +20,22 @@ describe('ApiService', () => {
 
   it('check genres', () => {
     const genres = spyOn(service, 'getGanres').and.callFake(() => {
-      return of(testGenres);
+      return of(testGenresMock);
     });
 
     service.getGanres().subscribe((genre) => {
-      expect(genre).toEqual(testGenres);
+      expect(genre).toEqual(testGenresMock);
     });
     expect(genres).toHaveBeenCalled();
   });
 
   it('check popular', () => {
     const movies = spyOn(service, 'requestPopularMovie').and.callFake(() => {
-      return of(testMovies);
+      return of(testMoviesMock);
     });
 
     service.requestPopularMovie().subscribe((movie) => {
-      expect(movie).toEqual(testMovies);
+      expect(movie).toEqual(testMoviesMock);
     });
     expect(movies).toHaveBeenCalled();
   });
